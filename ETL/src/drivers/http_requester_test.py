8d8c7c3f369665e47d44a12084b62b21c976c953
Eduardo Orsi <duduayr@gmail.com>
2023-12-02 10:21:27 -0300

    feat: Implementing html_collector

--- ETL/src/drivers/http_requester_test.py
+++ ETL/src/drivers/http_requester_test.py
@@ -3,7 +3,7 @@ from .http_requester import HttpRequester
 def test_request_from_page(requests_mock):
     response_context = "<h1>Teste Titulo</h1>"
     requests_mock.get("https://google.com.br", status_code=200, text=response_context)
-    http_requester = HttpRequester("https://google.com.br")
+    http_requester = HttpRequester("https://web.archive.org/web/20121007172955/http://www.nga.gov/collection/anZ1.htm")
     request_response = http_requester.request_from_page()
     print(request_response)
 
