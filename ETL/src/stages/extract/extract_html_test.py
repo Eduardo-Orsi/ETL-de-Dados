c3b6965980b3a114a32db51db0be824efd1694b9
Eduardo Orsi <duduayr@gmail.com>
2023-12-02 11:22:40 -0300

    feat: Implementing ExtractionError for extract stage

--- ETL/src/stages/extract/extract_html_test.py
+++ ETL/src/stages/extract/extract_html_test.py
@@ -1,5 +1,6 @@
 from ...drivers.html_collector import HtmlCollector
 from ...drivers.http_requester import HttpRequester
+from ..contracts.extract_contract import ExtractContract
 
 from .extract_html import ExtractHtml
 
@@ -11,4 +12,13 @@ def test_extract():
     extract_html = ExtractHtml(http_requester, html_collector)
     response = extract_html.extract()
     print()
-    print(response)
+    assert isinstance(response, ExtractContract)
+
+def test_extract_error():
+    http_requester = HttpRequester("https://web.archive.org/web/20121007172955/http://www.nga.gov/collection/anZ1.htm")
+    html_collector = HtmlCollector()
+
+    extract_html = ExtractHtml(http_requester, html_collector)
+    response = extract_html.extract()
+    print()
+    assert isinstance(response, ExtractContract)
