e8a447dd9343d2b8c0913f0b4bb71a18258d90f9
Eduardo Orsi <duduayr@gmail.com>
2023-12-03 17:26:04 -0300

    feat: Implementing TransformContract and TransformError

--- ETL/src/stages/transform/transform_raw_data.py
+++ ETL/src/stages/transform/transform_raw_data.py
@@ -1,13 +1,18 @@
 from typing import List, Dict
-
 from ..contracts.extract_contract import ExtractContract
+from ..contracts.transform_contract import TransformContract
+from ...errors.transform_error import TransformError
 
 
 class TransformRawData:
 
-    def transform(self, extract_contract: ExtractContract) -> List[Dict]:
-        transformed_information = self.__filter_and_transform_data(extract_contract)
-        return transformed_information
+    def transform(self, extract_contract: ExtractContract) -> TransformContract:
+        try:
+            transformed_information = self.__filter_and_transform_data(extract_contract)
+            transformed_data_contract = TransformContract(load_content=transformed_information)
+            return transformed_data_contract
+        except Exception as exception:
+            raise TransformError(str(exception)) from exception
 
     def __filter_and_transform_data(self, extract_contract: ExtractContract) -> List[Dict]:
         extraction_date = extract_contract.extraction_date
--- ETL/src/stages/transform/transform_raw_data_test.py
+++ ETL/src/stages/transform/transform_raw_data_test.py
@@ -1,14 +1,27 @@
+#pylint: disable=unused-variable
+#pylint: disable=broad-exception-caught
+
 from .transform_raw_data import TransformRawData
 from ..contracts.mocks.extract_contract import extract_contract_mock
+from ..contracts.transform_contract import TransformContract
+from ...errors.transform_error import TransformError
 
 def test_transform():
     transform_raw_data = TransformRawData()
     transformed_information = transform_raw_data.transform(extract_contract_mock)
-    assert isinstance(transformed_information, list)
-    assert isinstance(transformed_information[0], dict)
-    assert 'first_name' in transformed_information[0]
-    assert 'second_name' in transformed_information[0]
-    assert 'surname' in transformed_information[0]
-    assert 'artist_id' in transformed_information[0]
-    assert 'link' in transformed_information[0]
-    assert 'extraction_date' in transformed_information[0]
+    assert isinstance(transformed_information, TransformContract)
+    assert isinstance(transformed_information.load_content, list)
+    assert isinstance(transformed_information.load_content[0], dict)
+    assert 'first_name' in transformed_information.load_content[0]
+    assert 'second_name' in transformed_information.load_content[0]
+    assert 'surname' in transformed_information.load_content[0]
+    assert 'artist_id' in transformed_information.load_content[0]
+    assert 'link' in transformed_information.load_content[0]
+    assert 'extraction_date' in transformed_information.load_content[0]
+
+def test_transform_error():
+    transform_raw_data = TransformRawData()
+    try:
+        transformed_information = transform_raw_data.transform('entrada com erro')
+    except Exception as exception:
+        assert isinstance(exception, TransformError)
