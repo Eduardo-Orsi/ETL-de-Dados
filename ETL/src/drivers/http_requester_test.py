743ae673d748223fb67c687326f3374bf499819e
Eduardo Orsi <duduayr@gmail.com>
2023-12-04 22:52:11 -0300

    bug: Fixing HttpRequester test

--- ETL/src/drivers/http_requester_test.py
+++ ETL/src/drivers/http_requester_test.py
@@ -1,8 +1,9 @@
+#pylint: disable=line-too-long
 from .http_requester import HttpRequester
 
 def test_request_from_page(requests_mock):
     response_context = "<h1>Teste Titulo</h1>"
-    requests_mock.get("https://google.com.br", status_code=200, text=response_context)
+    requests_mock.get("https://web.archive.org/web/20121007172955/http://www.nga.gov/collection/anZ1.htm", status_code=200, text=response_context)
     http_requester = HttpRequester("https://web.archive.org/web/20121007172955/http://www.nga.gov/collection/anZ1.htm")
     request_response = http_requester.request_from_page()
     print(request_response)
